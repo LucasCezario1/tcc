39de1d9e9ab9ad280460426b92f7338574cf1729
Lucas Cezario <lucasdc96@gmail.com>
2023-10-01 12:22:27 -0300

    fix na simulacao do spacy

--- spacy/simulacao.py
+++ spacy/simulacao.py
@@ -1,9 +1,18 @@
-
-# SpaCy
 import spacy
-nlp = spacy.load('en_core_web_sm')
 
-text = "Este é um teste de tokenização com NLTK."
-doc = nlp(text)
-tokens = [token.text for token in doc]
-print(tokens)
+nlp = spacy.load('pt_core_news_sm')
+
+texto = "Bill Gates visitou a cidade de Nova York em 15 de julho de 2022 para uma conferência sobre inovação tecnológica."
+
+doc = nlp(texto)
+
+# Identificar as entidades nomeadas no texto
+detalhes_entidade = [(entidade, entidade.label_) for entidade in doc.ents]
+
+print("Identifica o que é uma localidade no texto: ", detalhes_entidade)
+
+
+#Pegar o unico digito do texto
+alpha_tokens = [token.orth_ for token in doc if token.is_digit]
+
+print("O digitos:" , alpha_tokens)
